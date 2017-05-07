88b44233f3fb57a309b06f03e90dcef577554f31
Genesys Source <developers@genesyssource.com>
2017-05-06 19:07:30 -0700

    2017.05 Iteration
    Summary of code changes in this iteration are:
    Genesys.Extensions
    • Genesys.Extensions.StringExtension.TryParseUri() string class
    extension method added.
    • Genesys.Extras.Serialization.JsonSerializer<T>
    o Default Json DateTime format now ISO8601 “F”:
    "yyyy-MM-dd'T'HH':'mm':'ss'.'fff"
    • Genesys.Extras.Net.HttpRequest<>: Constructors allow either Uri or
    string.
    Genesys.Foundation
    • Genesys.Foundation.Web, Windows, Universal: Introduced new projects of
    foundation classes to support web-based apps, windows-based apps and
    Universal/Xamarin-based apps.
    • Genesys.Foundation.Entity: Further refined the DAO foundation classes,
    and data-access helper classes.
    o CrudEntity<TEntity> and ReadOnlyEntity<TEntity>: Entity that supports
    CRUD operations against any datastore (DB, web service, xml document,
    etc.)
    o EntityWriter<TEntity>, EntityReader<TEntity>: Command objects for
    reading/writing an entity to a datastore.
    o SaveableDatabase, ReadOnlyDatabase: Entity Framework wrappers for
    consistent writing/reading interfaces in all tiers.
    Genesys Foundation for Web API, WPF and Universal
    • Foundation.WebServices
    o Default Web API Json Serializer changed to JSON.NET.
    DataContractJsonSerializer formatter is now 2nd in line.
    Quality Assurance
    • Added over 100 unit-tests to the various projects.
    • ISO8601 DateTime in Json is explicitly tested in all tiers, all
    projects.
    • Fakes added for Customer objects, to test CustomerInfo/CustomerModel
    in Extensions and Foundation solutions.
    DevOps
    • NuGet versioning now: 3.YY.MM.hhh
    •  Genesys assembly versioning: 3.YY.MM
    • Added post-release regression tests in an effort to ensure all
    products work first time, every time.
    • Release notes: http://bit.ly/2pVo4td
    • Setup Guide:
    http://docs.genesysframework.com/library/Genesys-Foundation-Quick-Guide.pdf
    
    We thank you for taking a look at our framework and code reuse
    solutions.
    - Genesys Source Dev Team
    
    Other sites of interest...
    Genesys Source Framework: http://www.getframework.com/
    Quick-Start Guide:
    http://docs.genesysframework.com/library/Genesys-Foundation-Quick-Guide.pdf
    Reference Docs: http://docs.genesysframework.com/
    Genesys Source on GitHub: http://www.github.com/GenesysSource/
    Genesys Source in Visual Studio: http://bit.ly/2mHpFkf
    
    Preview Disclaimer
    All Genesys software, services and source code provided, including the
    Genesys Framework Foundation, are currently in limited Preview, as
    outlined in our preview disclaimer notice available on
    http://www.GenesysFramework.com.

--- src/Foundation.WebServices/Properties/AssemblyInfo.cs
+++ src/Foundation.WebServices/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.17.04")]
-[assembly: AssemblyFileVersion("3.17.04")]
+[assembly: AssemblyVersion("3.17.05")]
+[assembly: AssemblyFileVersion("3.17.05")]
